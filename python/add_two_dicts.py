ce4469185bc0691afc66989da3efec92a6c06c26
Bharatkumarbv <chikkalabharatkumar@gmail.com>
2023-02-15 19:09:33 +0530

    move

---
+++ python/add_two_dicts.py
@@ -0,0 +1,5 @@
+d={"a":12, "b":23, "c":45}
+d1={"l":6, "m":4}
+for i,v in d1.items():
+    d[i]=v
+print(d)
---
+++ python/dict_one.py
@@ -0,0 +1,4 @@
+t={1: {'name': 'bharat', 'age': '25', 'profession': 'engineer',"lastname":"chikkala"}, 2: {'name': 'kumar', 'age': '26', 'profession': 'doctor',"lastname":"chikkala"}}
+t[1]['sandy']=24
+t[2]['vamsi']=23
+print(t)
